99a682df948c61ec5613aaa52c2698887d7c2dff
james.huang <james.huang@must.org.tw>
2021-08-18 12:08:48 +0800

    3

---
+++ leetcode/src/main/java/james/codercareer/LinkedList.java
@@ -0,0 +1,33 @@
+package james.codercareer;
+
+public class LinkedList {
+	public static void main(String[] args) {
+	}
+
+	private static void kthNodeFromEnd() {
+		/*
+		 * K-th Node from End Get the Kth node from end of a linked list. It counts from
+		 * 1 here, so the 1st node from end is the tail of list. For instance, given a
+		 * linked list with 6 nodes, whose value are 1, 2, 3, 4, 5, 6, its 3rd node from
+		 * end is the node with value 4.
+		 * 
+		 * A node in the list is defined as:
+		 * 
+		 * struct ListNode { int m_nValue; ListNode* m_pNext; };
+		 * 
+		 * Analysis: In a list with n nodes, its kth node from end should be the
+		 * (n-k+1)th node from its head. Therefore, if we know the number of nodes n in
+		 * a list, we can get the required node with n-k+1 steps from its head. How to
+		 * get the number n? It is easy if we scan the whole list from beginning to end.
+		 */
+
+	}
+	
+	private static void ReverseALinkedList() {
+		/*
+		 * Implement a function to reverse a linked list, and return the head of the
+		 * reversed list. A list node is defined as below: struct ListNode { int m_nKey;
+		 * ListNode* m_pNext; };
+		 */
+	}
+}
